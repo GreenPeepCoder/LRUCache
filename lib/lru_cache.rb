dde66cb856b5cebf302270bb4d749a6d4f085631
william_brooks_miller <brooksmiller.miller@gmail.com>
2020-04-19 23:50:16 -0400

    Outline the main methods of the LRU Cache, as well as the specs that will test them

---
+++ lib/lru_cache.rb
@@ -0,0 +1,21 @@
+# For this HW assignment, we're going to use an array to implement a basic LRU Cache
+# For this ex, we will NOT use a hash-map or a linked list
+# We will assume our input is limited to Integers, Strings, Arrays, Symbols, and Hashes
+# We will allow the user to set the size of the cache
+
+class LRU_Cache
+    def initialize (size = 4)
+        @size = size
+    end
+
+    def count
+    end
+
+    def add(ele)
+    end
+
+    def show
+    end
+
+    private
+end
---
+++ spec/lru_cache_spec.rb
@@ -0,0 +1,19 @@
+require 'rspec'
+require 'lru_cache.rb'
+
+describe LRU_Cache do
+
+    subject(:johnny_cache) {LRU_Cache.new}
+
+    describe "#initialize" do
+    end
+
+    describe "#count" do
+    end
+
+    describe "#add" do
+    end
+
+    describe "#show" do
+    end
+end
